f778bd6e7901c6ad0e5660fcf97b957310eada27
Procesor <jan.egermaier@gmail.com>
2024-02-25 19:29:25 +0100

    - more readable table with type of stamps

--- frontend/src/components/LowerSection.vue
+++ frontend/src/components/LowerSection.vue
@@ -9,18 +9,41 @@
     props: {
       stampTypes: Array,
     },
-    computed: {
-      columns() {
-        if (this.stampTypes.length === 0) {
-          return [];
-        }
-  
-        return Object.keys(this.stampTypes[0]).map((key) => ({
-          title: key,
-          dataIndex: key,
-          key,
-        }));
-      },
+    data() {
+      return {
+        columns: [
+          {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: 'Typee',
+            dataIndex: 'type',
+            key: 'type',
+          },
+          {
+            title: 'Color',
+            dataIndex: 'color',
+            key: 'color',
+          },
+          {
+            title: 'Perf',
+            dataIndex: 'perforation',
+            key: 'perforation',
+          },
+          {
+            title: 'Desk bug',
+            dataIndex: 'plate_flaw',
+            key: 'plate_flaw',
+          },
+          {
+            title: 'Catalog price',
+            dataIndex: 'catalog_price',
+            key: 'catalog_price',
+          },
+        ],
+      };
     },
   };
   </script>
