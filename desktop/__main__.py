d6306fc63c668fde38f2d80c5930802d59b1cd2f
Procesor <jan.egermaier@gmail.com>
2024-11-21 17:12:37 +0100

    - minor fix

--- desktop/__main__.py
+++ desktop/__main__.py
@@ -23,20 +23,12 @@ class EmissionScreen(Screen):
     pass
 
 class HoverButton(MDRoundFlatIconButton, HoverBehavior):
-    background = ListProperty((0/255, 0/255,0/255,1))
     color_text = 1,1,1,1
-    color_icon = [1,1,1,1]
     def on_enter(self):
-        self.background = (1, 222/255, 89/255, 1)
-        self.color = (0, 0, 0, 1)
         self.text_color = 0.89, 0.35, 0, 1
-        self.color_icon = [0.89, 0.35, 0, 1]
 
     def on_leave(self):
-        self.background = (0, 0, 0, 1)
-        self.color = (1, 1, 1, 1)
         self.text_color = 1,1,1,1
-        self.color_icon = [1,1,1,1]
 
 if __name__ == '__main__':
     app = MainApp()
