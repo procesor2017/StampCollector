af8eed0a69cf76000ca479194c5ac40c16b7e816
Procesor <jan.egermaier@gmail.com>
2024-11-21 17:12:01 +0100

    - add hover button

--- desktop/__main__.py
+++ desktop/__main__.py
@@ -4,6 +4,8 @@ from kivymd.uix.screen import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRoundFlatIconButton
 from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.behaviors import HoverBehavior
+from kivy.properties import ListProperty
 
 # Definování barevného schématu
 class MainApp(MDApp):
@@ -20,22 +22,21 @@ class HomeScreen(Screen):
 class EmissionScreen(Screen):
     pass
 
-class HoverItem(MDRoundFlatIconButton):
-    '''Custom item implementing hover behavior.'''
+class HoverButton(MDRoundFlatIconButton, HoverBehavior):
+    background = ListProperty((0/255, 0/255,0/255,1))
+    color_text = 1,1,1,1
+    color_icon = [1,1,1,1]
+    def on_enter(self):
+        self.background = (1, 222/255, 89/255, 1)
+        self.color = (0, 0, 0, 1)
+        self.text_color = 0.89, 0.35, 0, 1
+        self.color_icon = [0.89, 0.35, 0, 1]
 
-    def on_enter(self, *args):
-        '''The method will be called when the mouse cursor
-        is within the borders of the current widget.'''
-
-        self.md_bg_color = (1, 1, 1, 1)
-
-    def on_leave(self, *args):
-        '''The method will be called when the mouse cursor goes beyond
-        the borders of the current widget.'''
-
-        self.md_bg_color = self.theme_cls.bg_darkest
-
-
+    def on_leave(self):
+        self.background = (0, 0, 0, 1)
+        self.color = (1, 1, 1, 1)
+        self.text_color = 1,1,1,1
+        self.color_icon = [1,1,1,1]
 
 if __name__ == '__main__':
     app = MainApp()
