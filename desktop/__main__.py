644c68a4f41d2b06185de0c6726b122e8376882b
Procesor <jan.egermaier@gmail.com>
2024-11-21 16:42:36 +0100

    - update

--- desktop/__main__.py
+++ desktop/__main__.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.screen import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRoundFlatIconButton
 from kivy.uix.screenmanager import ScreenManager
 
 # Definování barevného schématu
@@ -18,6 +19,23 @@ class HomeScreen(Screen):
 
 class EmissionScreen(Screen):
     pass
+
+class HoverItem(MDRoundFlatIconButton):
+    '''Custom item implementing hover behavior.'''
+
+    def on_enter(self, *args):
+        '''The method will be called when the mouse cursor
+        is within the borders of the current widget.'''
+
+        self.md_bg_color = (1, 1, 1, 1)
+
+    def on_leave(self, *args):
+        '''The method will be called when the mouse cursor goes beyond
+        the borders of the current widget.'''
+
+        self.md_bg_color = self.theme_cls.bg_darkest
+
+
 
 if __name__ == '__main__':
     app = MainApp()
