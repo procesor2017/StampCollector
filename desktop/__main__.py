128feb844074b0681ef058839a358d0c0b58a586
Procesor <jan.egermaier@gmail.com>
2024-11-22 16:47:44 +0100

    - righ panel for stamp

--- desktop/__main__.py
+++ desktop/__main__.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivymd.uix.screen import Screen
+from kivymd.uix.screenmanager import ScreenManager
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDRoundFlatIconButton
 from kivymd.uix.behaviors import HoverBehavior
@@ -11,14 +12,17 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.animation import Animation
+from kivy.uix.behaviors import ButtonBehavior
 
 class MainApp(MDApp):
     def build(self):
         Builder.load_file('views/main.kv')
         return MainLayout()
 
-class MainLayout(MDBoxLayout):
-    pass
+class MainLayout(MDBoxLayout, Screen):
+    def show_righ_panel(self):
+        pass
 
 class HomeScreen(Screen):
     pass
@@ -26,7 +30,6 @@ class HomeScreen(Screen):
 class EmissionScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
         # Načítání zemí z CRUD
         self.countries = crud.get_all_countries()
         self.menu = None
@@ -82,12 +85,7 @@ class EmissionScreen(Screen):
                     pos_hint= {"center_x": .5, "center_y": .5},
                 )
             results_box.add_widget(expansionPanel)
-""" 
-    def on_stamp_selected(match, touch, instance):
-        if instance.collide_point(*touch.pos):
-            print(f"Selected stamp: {match.stamp_name} (ID: {match.stamp_id})")
-            # Zde lze přejít na detailní stránku známky
-            # Např. změna obrazovky nebo načtení detailu """
+
 
 class HoverButton(MDRoundFlatIconButton, HoverBehavior):
     color_text = 1,1,1,1
@@ -105,7 +103,7 @@ from kivy.uix.image import AsyncImage
 from kivy.uix.gridlayout import GridLayout
 
 class Content(MDBoxLayout):
-    def __init__(self, emission, **kwargs):
+    def __init__(self, emission,**kwargs):
         super().__init__(**kwargs)
         self.orientation = "vertical"
         self.adaptive_height = True
@@ -113,7 +111,7 @@ class Content(MDBoxLayout):
 
         # GridLayout pro zalamování obrázků
         grid_layout = GridLayout(
-            cols=3,  # Počet sloupců (můžeš změnit podle potřeby)
+            cols=3,
             row_default_height="150dp",
             row_force_default=True,
             spacing="10dp",  # Mezera mezi obrázky
@@ -136,15 +134,14 @@ class Content(MDBoxLayout):
                 text=match.name,
             )
             grid_layout.add_widget(image_with_text)
-
         self.add_widget(grid_layout)
 
 
-class ImageWithText(RelativeLayout):
+class ImageWithText(ButtonBehavior, RelativeLayout):
     def __init__(self, image_source, text, **kwargs):
         super().__init__(**kwargs)
         self.size_hint = (None, None)
-        self.size = ("150dp", "150dp") 
+        self.size = ("150dp", "150dp")
 
         # Obrázek
         self.image = AsyncImage(
@@ -164,26 +161,33 @@ class ImageWithText(RelativeLayout):
             text_color=(1, 1, 1, 1),
             bold=True,
         )
+
         self.add_widget(self.label)
 
+    def on_release(self):
+        app = MDApp.get_running_app()
+        right_panel = app.root.ids.right_panel
+        panel_label = app.root.ids.panel_label  # Label, kam se zobrazují informace
 
+        # Získat aktuální text v panelu
+        current_text = panel_label.text
+        new_text = self.label.text  # Nový text na základě aktuální známky
 
+        if right_panel:
+            if right_panel.size_hint_x == 0:  # Pokud je panel skrytý, zobrazí ho
+                animation = Animation(size_hint_x=0.3, duration=0.3)
+                animation.start(right_panel)
+                panel_label.text = new_text  # Aktualizace textu
+            else:
+                if current_text != new_text:  # Pokud se data změnila, aktualizujte je
+                    panel_label.text = new_text
+                else:  # Pokud se data nezměnila, zavřete panel
+                    animation = Animation(size_hint_x=0, duration=0.3)
+                    animation.start(right_panel)
+        else:
+            print("Right panel not found!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 if __name__ == '__main__':
